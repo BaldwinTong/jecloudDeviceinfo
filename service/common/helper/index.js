d7d1bbdc3166a0de1982d75029428adae67de525
linzhichao <linzc@ketr.com.cn>
2022-01-14 11:37:12 +0800

    fix(css): 统一调整antd个性化样式引入方式

--- service/common/helper/index.js
+++ service/common/helper/index.js
@@ -5,7 +5,6 @@ import { setupIi8n } from '../locales';
 
 export async function setupCommon(vue) {
   import('@common/assets/styles/index.less');
-  import('@jecloud/ui/src/assets/fonts/index.css');
   // Store
   setupStore(vue);
   // JE
