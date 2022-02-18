92a29e8b69eb7a5c9797aa377fca8c3feba0fd85
linzhichao <linzc@ketr.com.cn>
2022-02-18 18:05:46 +0800

    feat(ant): 更新antd版本3.0.0-beta.9

--- service/micro/index.js
+++ service/micro/index.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue';
 import { setupCommon } from '@common/helper';
 import { setupRouter } from './router';
 import { useMicroHooks } from './hooks/use-micro';
+import { setup as uiSetup } from '@jecloud/ui';
 import './helper/public-path';
 
 let _app; // 应用入口
@@ -35,6 +36,8 @@ async function setup({ app, router = true, callback }) {
 async function render(container) {
   // Init Vue
   const vue = createApp(_app);
+  // 注册 Vue
+  uiSetup(vue);
   // Common
   await setupCommon(vue);
   // Router
