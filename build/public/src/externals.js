e3df4bc77cdbd6444cc3f6d6b77cafbf0117ce6a
linzhichao <linzc@ketr.com.cn>
2022-05-04 00:23:21 +0800

    fix(style): 修改style文件引用路径[#8]

--- build/public/src/externals.js
+++ build/public/src/externals.js
@@ -1,9 +1,9 @@
 /* 本文件供开发环境使用，webpack打包，会排除改文件内容，单独打包 */
 
 // ant-design-vue
-import 'ant-design-vue/dist/antd.less';
+import './styles/ant-design-vue';
 // vxe-table
-import 'vxe-table/es/style.min.css';
+import './styles/vxe-table';
 // 公共样式
 import './styles/common';
 // 字体图标
