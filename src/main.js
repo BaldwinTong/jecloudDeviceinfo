6d1aea5c6719b6115ffe84a89c6f9b0c28861267
linzhichao <linzc@ketr.com.cn>
2022-06-08 10:21:18 +0800

    feat(workflow): 增加@jecloud/worfkow[#8]

--- src/main.js
+++ src/main.js
@@ -3,13 +3,14 @@ import { useJE } from '@common/helper/je';
 import micro from '@micro';
 import ui, { ConfigProvider } from '@jecloud/ui';
 import func from '@jecloud/func';
+import workflow from '@jecloud/workflow';
 import App from './app.vue';
 
 micro.setup({
   app: App,
   callback: (vue) => {
     // vue 实例，可以进行相关业务操作
-    vue.use(ui).use(func);
+    vue.use(ui).use(func).use(workflow);
     // 配置UI库，如果不需要功能，可以不引入
     ConfigProvider.setup({ publicPath: CLI_ENVS.PUBLIC_PATH, func });
     // 功能组件绑定JE，用于事件操作
