8a20fe0fb4a0a3022dce708c5fe28ed9ebefbd5a
linzhichao <linzc@ketr.com.cn>
2022-08-12 14:54:46 +0800

    fix(build): 调整项目打包配置[#8]

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -1,15 +1,12 @@
 const fs = require('fs');
-const path = require('path');
 const {resolve} = require('./build/utils');
 const libPrefix = 'jecloud';
-const libs = ['@jecloud/ui','@jecloud/utils'];
 // 增加开发依赖
 function readPackage(pkg, context) {
   if (pkg.name.startsWith(libPrefix)) {
-    libs.forEach((name)=>{
-      const lib = pkg.dependencies[name];
-      if(lib && lib.startsWith('file:.yalc')){
-        const package = resolve(lib.replace('file:','')+'/package.json');
+    Object.keys(pkg.dependencies).forEach((lib)=>{
+      if(lib.startsWith('@jecloud/')){
+        const package = resolve(`.yalc/${lib}/package.json`);
         const data = JSON.parse(fs.readFileSync(package).toString());
         pkg.dependencies = Object.assign(data.dependencies,pkg.dependencies);
       }
